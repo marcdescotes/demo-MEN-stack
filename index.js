d127593508477a68e6f2663fe608b20f2daa0eff
Marc-André Descôteaux <marcdescotes@gmail.com>
2023-07-03 12:54:29 -0400

    Ajout .env + ajout URI BD

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connection à MongoDB
-mongoose.connect(process.env.ATLASDB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connexion à MongoDB réussie !");
   })
