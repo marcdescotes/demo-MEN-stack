2dccda790146b509ae7bcab4216fbcd6779f075f
Marc-André Descôteaux <marcdescotes@gmail.com>
2023-07-03 19:14:48 -0400

    added authenticator token

--- controllers/User.js
+++ controllers/User.js
@@ -27,9 +27,16 @@ exports.login = (req, res, next) => {
                     if (!valid) {
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' })
                     }
+
+                    const token = jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' }
+                        );
+                    
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        token: token
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
